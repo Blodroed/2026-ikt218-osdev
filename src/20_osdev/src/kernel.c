df8b6b45bb4a8377d3f10942a6c7adee47f8fca6
Matin Feizi <matinf@uia.no>
2026-04-26 21:45:57 +0200

    Assignment 3: Interrupts (IDT, ISR, IRQ, Keyboard)

--- src/20_osdev/src/kernel.c
+++ src/20_osdev/src/kernel.c
@@ -1,17 +1,31 @@
 #include <libc/stdint.h>
 #include "gdt.h"
+#include "idt.h"
+#include "isr.h"
+#include "irq.h"
 
-void main(void)
+static volatile uint16_t *video = (volatile uint16_t *)0xB8000;
+
+static void print(const char *text)
 {
-    gdt_init();
-
-    volatile uint16_t *video = (volatile uint16_t *)0xB8000;
-    const char *text = "Hello World";
-
     for (int i = 0; text[i] != '\0'; i++)
     {
         video[i] = (uint16_t)text[i] | (uint16_t)0x0F00;
     }
+}
+
+void main(void)
+{
+    gdt_init();
+    idt_init();
+    isr_init();
+    irq_init();
+
+    print("GDT, IDT, ISR and IRQ loaded");
+
+    __asm__ volatile("int $0x0");
+    __asm__ volatile("int $0x1");
+    __asm__ volatile("int $0x2");
 
     while (1)
     {
